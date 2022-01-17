a6567c46b374a12c2c05169b22201ffae25dbe64
nobucastle <30787155+nobucastle@users.noreply.github.com>
2022-01-17 13:32:44 +0900

    20220104

--- config/config.php
+++ config/config.php
@@ -3,8 +3,8 @@
 ini_set('display_errors', 1);
 ini_set("soap.wsdl_cache_enabled", "0");
 
-require_once("/lib/functions.php");
-require_once("/autoload.php");
+require_once("lib/functions.php");
+require_once("autoload.php");
 
 //セールスフォース接続用設定ファイル
 require_once ("ForceComToolkitForPHPmaster/soapclient/SforcePartnerClient.php");
