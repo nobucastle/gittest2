9e2e39c41654286c8953c378195a9c054349cc08
nobucastle <30787155+nobucastle@users.noreply.github.com>
2022-01-17 14:02:34 +0900

    20220116

--- config/config.php
+++ config/config.php
@@ -7,8 +7,8 @@ require_once('/lib/functions.php');
 require_once('/autoload.php');
 
 //セールスフォース接続用設定ファイル
-require_once ('ForceComToolkitForPHPmaster/soapclient/SforcePartnerClient.php');
-require_once ('ForceComToolkitForPHPmaster/soapclient/SforceHeaderOptions.php');
+require_once ('/ForceComToolkitForPHPmaster/soapclient/SforcePartnerClient.php');
+require_once ('/ForceComToolkitForPHPmaster/soapclient/SforceHeaderOptions.php');
 
 //データベース定義
 define('DB_HOST_SENKYO', 'SRVSC1');
